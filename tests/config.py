16d303b731bc4cd2db92675103a26807ff96036e
V1aj3ro <viajero95@mail.ru>
2026-08-27 00:43:10 +0700

    HTTP мок-сервис: интеграция в тестовый стенд

--- tests/config.py
+++ tests/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from tests.tools.config.grpc import GRPCClientTestConfig, GRPCServerTestConfig
 from tests.tools.config.http import HTTPClientTestConfig, HTTPServerTestConfig
 from tests.tools.config.kafka import KafkaClientTestConfig
+from tests.tools.config.postgres import PostgresClientTestConfig
 
 
 class TestSettings(BaseSettings):
