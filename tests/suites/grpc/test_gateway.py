f381a0656b61c938d822c44a867e65e330a79618
V1aj3ro <viajero95@mail.ru>
2026-08-29 16:47:47 +0700

    Сценарные тесты gRPC API. Сценарные тесты gRPC API

--- tests/suites/grpc/test_gateway.py
+++ tests/suites/grpc/test_gateway.py
@@ -3,6 +3,7 @@ import pytest
 
 from tests.assertions.grpc.gateway import (
     assert_get_user_details_response_user_with_active_credit_card_account,
+    assert_get_account_details_response_user_with_active_debit_card_account,
 )
 from tests.clients.grpc.gateway.client import GatewayGRPCTestClient
 from tests.context.base import RequestContext
@@ -26,3 +27,12 @@ class TestGatewayGRPC:
         )
 
         assert_get_user_details_response_user_with_active_credit_card_account(response)
+
+    def test_get_account_details_user_with_active_debit_card_account(
+            self,
+            gateway_grpc_test_client: GatewayGRPCTestClient
+    ):
+        response = gateway_grpc_test_client.get_account_details(
+            RequestContext(scenario=Scenario.USER_WITH_ACTIVE_DEBIT_CARD_ACCOUNT)
+        )
+        assert_get_account_details_response_user_with_active_debit_card_account(response)
